4680949a2336058738f070f915ea81ebed5d942f
arthurbeltran <arthurtbeltran@gmail.com>
2015-09-28 16:43:27 -0400

    a
    a

---
+++ hector/tickets3/main_menu.php
@@ -0,0 +1,22 @@
+<div class="section text-center" style="padding:10px; background-color:aliceblue;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <ul class="nav nav-pills navbar-nav">
+              <li class="active">
+                <a href="index.html"><i class="fa fa-fw fa-home"></i> Home</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-info fa-fw"></i> About</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-envelope fa-fw"></i> Contact Us</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-search fa-fw"></i> Search</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</div>
