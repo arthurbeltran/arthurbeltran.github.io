192175be9242a2a937df48baf2655a47affcd6ea
arthurbeltran <arthurtbeltran@gmail.com>
2017-04-13 13:01:21 -0400

    added span

--- css/github_interactive_styles2/MxAO_nonumbering.js
+++ css/github_interactive_styles2/MxAO_nonumbering.js
@@ -161,7 +161,7 @@ function odu_i_formatArray(odu_i_arr){//array value formatting cb, fi and fb
         odu_i_outStr = odu_i_arr.join(' and ');
     } else if (odu_i_arr.length > 2) {
         //joins all with commas, but last one gets ", and"        
-        odu_i_outStr = odu_i_arr.slice(0, -1).join(', ') + ', and ' + odu_i_arr.slice(-1);
+        odu_i_outStr = odu_i_arr.slice(0, -1).join(', ') + ', <span style="font-weight:100">and</span> ' + odu_i_arr.slice(-1);
     }
     return odu_i_outStr;
 }//end of array value formatting cb and fi            
