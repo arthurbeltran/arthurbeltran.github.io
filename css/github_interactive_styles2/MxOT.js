ba86f4dfcfafcd1720a7e38d60e752cb6f82f5c4
arthurbeltran <arthurtbeltran@gmail.com>
2017-04-21 16:47:43 -0400

    changed the "and"

--- css/github_interactive_styles2/MxOT.js
+++ css/github_interactive_styles2/MxOT.js
@@ -8,19 +8,19 @@ var odu_i_notification_validate = '<div class="odu_i_validate"><span class="odu_
     
 $(document).ready(function(){//start of document ready    
     
-function odu_i_formatArray(odu_i_arr){//array value formatting cb and fi
+function odu_i_formatArray(odu_i_arr){//array value formatting cb, fi and fb
     var odu_i_outStr = '';
-    if (odu_i_arr.length === 1) { 
+    if (odu_i_arr.length === 1) {
         odu_i_outStr = odu_i_arr[0];
     } else if (odu_i_arr.length === 2) {
         //joins all with "and" but no commas        
-        odu_i_outStr = odu_i_arr.join(' and ');
+        odu_i_outStr = odu_i_arr.join(' <span style="font-weight:100">and</span> ');
     } else if (odu_i_arr.length > 2) {
         //joins all with commas, but last one gets ", and"        
-        odu_i_outStr = odu_i_arr.slice(0, -1).join(', ') + ', and ' + odu_i_arr.slice(-1);
+        odu_i_outStr = odu_i_arr.slice(0, -1).join(', ') + ', <span style="font-weight:100">and</span> ' + odu_i_arr.slice(-1);
     }
     return odu_i_outStr;
-}//end of array value formatting cb and fi            
+}//end of array value formatting cb and fi          
     
 function odu_i_compareArrays(odu_i_arr1, odu_i_arr2) {//compare simple arrays for cb and fi
     return $(odu_i_arr1).not(odu_i_arr2).length == 0 && $(odu_i_arr2).not(odu_i_arr1).length == 0
